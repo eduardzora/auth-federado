bdfa274ce0dec7ecf6d22c7c1a05e602cf80781e
Eduard Zora <norbey-zora07@hotmail.com>
2022-03-20 17:08:23 -0500

    auth-federado firs commit

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const GOOGLE_CLIENT_ID =
+  process.env.GOOGLE_CLIENT_ID ||
+  "309609637878-9ide5q9upk8sk95kljv63cjvldcbkn7i.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_SECRET =
+  process.env.GOOGLE_CLIENT_SECRET || "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";
+export const SERVER_ROOT_URI = "http://localhost:4000";
+export const UI_ROOT_URI = "http://localhost:3000";
+export const JWT_SECRET = "shhhhh";
+export const COOKIE_NAME = "auth_token";
